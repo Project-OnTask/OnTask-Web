ff5a56189b55e059d382f6da957147d2d83082a3
pavinduLakshan <174084F@uom.lk>
2019-11-12 13:42:35 +0530

    set pusher logging only on development

--- src/utils/PusherObject.js
+++ src/utils/PusherObject.js
@@ -1,11 +1,10 @@
-import Pusher from 'pusher-js'
+import Pusher from "pusher-js";
 
-Pusher.logToConsole = true;
-  
+Pusher.logToConsole = process.env.REACT_APP_NODE_ENV === "production" ? false : true;
 
-      var pusher = new Pusher('1f56dbce61dcd7d6da58', {
-        cluster: 'ap2',
-        encrypted: true
-      })
+var pusher = new Pusher("1f56dbce61dcd7d6da58", {
+  cluster: "ap2",
+  encrypted: true,
+});
 
 export default pusher;
